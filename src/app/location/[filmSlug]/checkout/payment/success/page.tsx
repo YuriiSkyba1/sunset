00fa91c4fb1db85b3797c64a7a7d2153b6552917
YuriiSkyba1 <yuraaa1111@gmail.com>
2024-04-28 22:21:04 +0300

    update request

--- src/app/location/[filmSlug]/checkout/payment/success/page.tsx
+++ src/app/location/[filmSlug]/checkout/payment/success/page.tsx
@@ -1,32 +1,29 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import HeaderCheckout from "@/components/HeaderCheckout/HeaderCheckout";
 import PaymentSuccessPopUp from "@/components/PaymentSuccessPopUp/PaymentSuccessPopUp";
-import React, { useState, useEffect } from "react";
-// import { useRouter } from "next/navigation";
 
 export default function page() {
 	const [isSuccess, setOpenSuccess] = useState(false);
+	const router = useRouter();
 
-	// const router = useRouter();
-console.log('111')
-	// const { order_id } = router.query;
+console.log('111', router)
 
-	// Extract the order_id from the query parameters
-	const searchParams = new URLSearchParams(window.location.search);
 
-	useEffect(() => {
-		console.log("searchParams with JS", searchParams);
-	}, []);
+useEffect(() => {
+	console.log('trigger use effect')
+    if (typeof window !== "undefined") {
+      const { order_id } = router.query;
+      console.log("Order ID: ", order_id);
+    
+    }
+  }, [router.isReady, router.query]);
 
-	// useEffect(() => {
-	// 	console.log("order_id with next", order_id);
-	// 	handleStatusCheck(order_id as unknown as number);
-	// }, [order_id]);
-
-	const handleStatusCheck = async (order_id: number = 24)  => {
+	const handleStatusCheck = async (order_id)  => {
 		const url = `/api/checkStatus`;
-		console.log("Attempting to fetch:", url); // Check the URL is correct
+		console.log("Attempting to fetch:", url); 
 		try {
 			const response = await fetch(url, {
 				method: "GET",
@@ -34,7 +31,7 @@ console.log('111')
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({ order_id }),
-				credentials: "include", // Ensures cookies are included with the request
+				credentials: "include", 
 			});
 
 			if (!response.ok) {
