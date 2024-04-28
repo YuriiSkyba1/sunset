8428772088bcd0c4436705ca8faa8720825019bf
YuriiSkyba1 <yuraaa1111@gmail.com>
2024-04-28 21:29:43 +0300

    fix ticket redirect

--- src/app/location/[filmSlug]/checkout/payment/success/page.tsx
+++ src/app/location/[filmSlug]/checkout/payment/success/page.tsx
@@ -2,10 +2,52 @@
 
 import HeaderCheckout from "@/components/HeaderCheckout/HeaderCheckout";
 import PaymentSuccessPopUp from "@/components/PaymentSuccessPopUp/PaymentSuccessPopUp";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function page() {
-	const [isSuccess, setOpenSuccess] = useState(true);
+	const [isSuccess, setOpenSuccess] = useState(false);
+
+	const router = useRouter();
+
+	const { order_id } = router.query;
+
+	// Extract the order_id from the query parameters
+	const searchParams = new URLSearchParams(window.location.search);
+
+	useEffect(() => {
+		console.log("searchParams with JS", searchParams);
+	}, []);
+
+	useEffect(() => {
+		console.log("order_id with next", order_id);
+		handleStatusCheck(order_id as unknown as number);
+	}, [order_id]);
+
+	const handleStatusCheck = async (order_id: number) => {
+		const url = `/api/checkStatus`;
+		console.log("Attempting to fetch:", url); // Check the URL is correct
+		try {
+			const response = await fetch(url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ order_id }),
+				credentials: "include", // Ensures cookies are included with the request
+			});
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			} else {
+				const data = await response.json();
+				console.log("Response data222 of checking status:", data);
+			}
+		} catch (error) {
+			console.error("Error checking status:", error);
+			return false;
+		}
+	};
 
 	return (
 		<div className="relative">
