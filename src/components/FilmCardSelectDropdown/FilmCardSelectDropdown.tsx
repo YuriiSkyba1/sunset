2a03e4bcdf672fa25b7dba772ee7fe29f0f4ad9b
YuriiSkyba1 <yuraaa1111@gmail.com>
2024-06-24 17:12:17 +0300

    fixed ui issue

--- src/components/FilmCardSelectDropdown/FilmCardSelectDropdown.tsx
+++ src/components/FilmCardSelectDropdown/FilmCardSelectDropdown.tsx
@@ -97,7 +97,7 @@ function FilmCardSelectDropdown({
         onClick={() => setIsOpen(!isOpen)}
       >
         <span>{selectedValue ? formatDateString(selectedValue) : label}</span>
-        <div className={`${isOpen && isActive ? "" : "rotate-180"}`}>
+        <div className={`${isOpen && isActive ? "rotate-180" : ""}`}>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               fillRule="evenodd"
